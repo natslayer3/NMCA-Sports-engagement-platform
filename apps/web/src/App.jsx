bec6eac6e18a211335e82fb8a084f3057b408395
DanielaHG <A00838702@tec.mx>
2026-03-12 03:05:20 -0600

    Frontend StorePage V1.1(Add store and successful payment page)

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import MatchesPage from "./pages/MatchesPage";
 import MatchRoomPage from "./pages/MatchRoomPage";
+import StorePage from "./pages/StorePage";
+import PaySuccess from "./pages/paySuccess";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Route path="/" element={<MatchesPage />} />
       <Route path="/matches" element={<MatchesPage />} />
       <Route path="/matches/:id" element={<MatchRoomPage />} />
+      <Route path="/store" element={<StorePage />} />
+      <Route path="/paySuccess" element={<PaySuccess />} />
     </Routes>
   );
 }
---
+++ apps/web/src/pages/StorePage.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import { getProducts, createCheckout } from '../services/storeService';
+import { useNavigate } from 'react-router-dom';
+
+export default function StorePage() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [quantity, setQuantity] = useState(1);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getProducts()
+      .then((res) => setProducts(res.products))
+      .catch(console.error)
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) return <p>Cargando productos...</p>;
+  console.log("Productos obtenidos:", products);
+  return (
+    <div>
+      <button onClick={() => navigate("/")}>Atras</button>
+      <h1>Tienda</h1>
+      <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(250px, 1fr))', gap: '1rem' }}>
+        {products && products.map((item) => (
+          <div key={item.id} style={{ border: '1px solid #ccc', padding: '1rem', borderRadius: '8px' }}>
+            {item.images?.[0] && (
+              <img src={item.images[0]} alt={item.name} style={{ width: '100%' }} />
+            )}
+            <h3>{item.name}</h3>
+            <p>{item.description}</p>
+            <p>${item.default_price}</p>
+            <label for="cantidad">Cantidad: </label>
+            <input type='number' id='cantidad' name='cantidad' min={1} step={1} onChange={(e) => setQuantity(e.target.value)}/>
+            <button onClick={() => createCheckout(item.default_price, quantity)}>
+              Comprar
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
